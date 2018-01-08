98af4af5d0116bb964f0dfdc8e8fc32851d41ea1
mkxzy <makina67@sina.com>
2018-01-08 23:20:35 +0800

    增加注释

--- cookie.py
+++ cookie.py
@@ -1,10 +1,12 @@
+# _*_ coding: utf-8 _*_
+
 from thinkbayes import Pmf
 
 pmf = Pmf()
-pmf.Set('Bow1', 0.5)
-pmf.Set('Bow2', 0.5)
-pmf.Mult('Bow1', 0.75)
-pmf.Mult('Bow2', 0.5)
-pmf.Normalize()
-t = pmf.Prob('Bow1')
+pmf.Set('Bow1', 0.5) #碗1的概率
+pmf.Set('Bow2', 0.5) #碗2的概率
+pmf.Mult('Bow1', 0.75) #碗1香草曲奇概率
+pmf.Mult('Bow2', 0.5) #碗2香草曲奇概率
+pmf.Normalize() #归一化
+t = pmf.Prob('Bow1') #后验概率
 print(t)
